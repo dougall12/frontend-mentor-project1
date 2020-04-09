9d7f7a2623a5ee324db5ebd75e73e517ea437ecc
dougall12 <s.dougall12@hotmail.co.uk>
2020-04-09 12:20:46 +0100

    Completed Project

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const toggleSwitch = document.getElementById("slider");
+const html = document.querySelector("html");
 
 toggleSwitch.addEventListener("click", () => {
   toggleSwitch.classList.toggle("dark");
+  html.classList.toggle("dark");
 });
